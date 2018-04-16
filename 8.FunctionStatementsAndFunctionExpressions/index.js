72b2c957d73aa1e9cc3eee0bc409b71bfae357d1
BorislavBorisov22 <borislavborisov22@abv.bg>
2018-04-17 00:13:21 +0300

    added function statements and function exressions exmaple

---
+++ 8.FunctionStatementsAndFunctionExpressions/index.js
@@ -0,0 +1,9 @@
+// statement because it does not result in a value
+function greet() {
+    console.log('Hi');
+}
+
+// expression because the function results in a value
+const anonymousGreet = function() {
+    console.log('Hi');
+};
