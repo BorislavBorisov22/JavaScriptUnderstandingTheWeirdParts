e3de54c4e183cdc2b19b857f2f989b944b85e7ef
Borislav Borisov <borislavborisov22@abv.bg>
2018-04-20 23:28:12 +0300

    removed misleading comments

--- 9.CallApplyBind/index.js
+++ 9.CallApplyBind/index.js
@@ -1,9 +1,8 @@
-// statement because it does not result in a value
+
 function greet() {
     console.log('Hi');
 }
 
-// expression because the function results in a value
 const anonymousGreet = function() {
     const args = arguments;
     const arrayArgs = [].slice.call(arguments);
